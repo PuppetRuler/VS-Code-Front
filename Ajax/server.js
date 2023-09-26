a535d95c4168372df0e129a44fabe87beac9dd7a
六十三 <12355712+hyh1850516439@user.noreply.gitee.com>
2023-09-26 08:08:16 +0800

    2023/9/26 submit

--- Ajax/server.js
+++ Ajax/server.js
@@ -79,6 +79,56 @@ app.all('/axios-server', (request, response) => {
     response.send(JSON.stringify(data));
 });
 
+//fetch服务
+app.all('/fetch-server', (request, response) => {
+    //设置响应头    设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    //响应头
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    //设置响应体
+    const data = { name: '尚硅谷' };
+    response.send(JSON.stringify(data));
+});
+
+//jsonp服务
+app.all('/jsonp-server', (request, response) => {
+    const data = { name: '尚硅谷' };
+    let str = JSON.stringify(data)
+    response.end(`handle(${str})`)  /* 通过服务端响应JS代码 */
+});
+
+//username服务
+app.all('/check-username', (request, response) => {
+    const data = {
+        exist:1,
+        msg:'用户名已存在'
+    };
+    let str = JSON.stringify(data)
+    response.end(`handle(${str})`)  /* 通过服务端响应JS代码 */
+});
+
+//jquery-jsonp-server服务
+app.all('/jquery-jsonp-server', (request, response) => {
+    const data = {
+        name:'尚硅谷',
+        address:['北京','深圳','上海']
+    };
+    let str = JSON.stringify(data)
+    //接收callback参数
+    const cb = request.query.callback
+    response.end(`${cb}(${str})`)  /* 通过服务端响应JS代码 */
+});
+
+//cors服务
+app.all('/cors-server',(req,res)=>{
+    //设置请求头
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Headers','*')
+    res.setHeader('Access-Control-Allow-Method','*')
+    // res.setHeader('Access-Control-Allow-Origin','http://127.0.0.1:5000')
+    res.send('hello cors')
+})
+
 //4. 监听端口启动服务
 app.listen(8000, () => {
     console.log("服务已经启动, 8000 端口监听中....");
